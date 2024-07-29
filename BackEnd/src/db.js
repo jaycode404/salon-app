d28587290f08217f4966e49a54777f639323dfad
FLRSscript <flrsscript@users.noreply.github.com>
2024-07-29 10:06:32 -0600

    responsive

--- BackEnd/src/db.js
+++ BackEnd/src/db.js
@@ -1,9 +1,9 @@
 import { createPool } from "mysql2/promise";
-
+import { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD, DB_PORT } from "./config.js";
 export const pool = createPool({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "salon-app",
-  port: "3306",
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  port: DB_PORT,
 });
