c7393d19fbf92f715f10080d232293cc5f6a63a1
FLRSscript <flrsscript@users.noreply.github.com>
2024-07-30 15:29:48 -0600

    deploy

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -15,7 +15,7 @@ const corsOptions = {
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 //KEYS//////////////////////////////////
--- FrontEnd/src/context/GeneralContext.jsx
+++ FrontEnd/src/context/GeneralContext.jsx
@@ -7,7 +7,7 @@ const GeneralProvider = ({ children }) => {
   const [citas, setCitas] = useState([]);
   const [allCitas, setAllCitas] = useState([]);
   const [loading, setLoading] = useState(true);
-  const dbUrl = import.meta.env.VITE_BACKEND_HOST;
+  const dbUrl = import.meta.env.VITE_BACKEND_HOST || 'http://localhost:3000';
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
     if (storedUser) {
