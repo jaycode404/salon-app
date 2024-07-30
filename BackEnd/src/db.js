2a08f3eaaf247520a8fc0fa1144337bb87734978
FLRSscript <flrsscript@users.noreply.github.com>
2024-07-30 16:53:45 -0600

    deploy

--- BackEnd/src/db.js
+++ BackEnd/src/db.js
@@ -1,10 +1,9 @@
 import { createPool } from "mysql2/promise";
 import { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD, DB_PORT } from "./config.js";
 export const pool = createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  port: DB_PORT,
 });
