9653492cefd0c3a0425689901ca1fb2efb59203d
FLRSscript <flrsscript@users.noreply.github.com>
2024-07-30 16:18:17 -0600

    deploy

--- BackEnd/src/db.js
+++ BackEnd/src/db.js
@@ -1,9 +1,9 @@
 import { createPool } from "mysql2/promise";
 import { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD, DB_PORT } from "./config.js";
 export const pool = createPool({
-    host: 'monorail.proxy.rlwy.net',
+  host: "monorail.proxy.rlwy.net",
   port: 32340,
-  user: 'root',
-  password: 'cqufjoYjagdBkKqcVefYPgLsbxyiPFXd',
-  database: 'railway'
+  user: "root",
+  password: "cqufjoYjagdBkKqcVefYPgLsbxyiPFXd",
+  database: "railway",
 });
