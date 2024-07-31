6264df375144915440fa3bd301d40796ad2b9b9c
FLRSscript <flrsscript@users.noreply.github.com>
2024-07-31 10:31:32 -0600

    new credentials

--- BackEnd/src/db.js
+++ BackEnd/src/db.js
@@ -1,15 +1,15 @@
-// import { createPool } from "mysql2/promise";
-// import { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD, DB_PORT } from "./config.js";
-// export const pool = createPool({
-//   host: DB_HOST,
-//   user: DB_USER,
-//   password: DB_PASSWORD,
-//   database: DB_NAME,
-//   port: DB_PORT,
-// });
 import { createPool } from "mysql2/promise";
-import { DB_URL } from "./config.js";
-
+import { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD, DB_PORT } from "./config.js";
 export const pool = createPool({
-  uri: DB_URL || `mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  port: DB_PORT,
 });
+// import { createPool } from "mysql2/promise";
+// import { DB_URL } from "./config.js";
+
+// export const pool = createPool({
+//   uri: DB_URL || `mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
+// });
