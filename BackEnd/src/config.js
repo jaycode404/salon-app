38f65af82893ac925d9134cb2b546cc887855099
FLRSscript <flrsscript@users.noreply.github.com>
2024-07-31 10:38:16 -0600

    new credentials

--- BackEnd/src/config.js
+++ BackEnd/src/config.js
@@ -1,9 +1,15 @@
 export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_USER = process.env.DB_USER || "root";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "root";
-export const DB_NAME = process.env.DB_NAME || "salon-app";
-export const DB_PORT = process.env.DB_PORT || 3306;
+export const MYSQLHOST = process.env.MYSQLHOST || "localhost";
+export const MYSQLUSER = process.env.MYSQLUSER || "root";
+export const MYSQLPASSWORD = process.env.MYSQLPASSWORD || "root";
+export const MYSQLDATABASE = process.env.MYSQLDATABASE || "salon-app";
+export const MYSQLPORT = process.env.MYSQLPORT || 3306;
+// export const PORT = process.env.PORT || 3000;
+// export const DB_HOST = process.env.DB_HOST || "localhost";
+// export const DB_USER = process.env.DB_USER || "root";
+// export const DB_PASSWORD = process.env.DB_PASSWORD || "root";
+// export const DB_NAME = process.env.DB_NAME || "salon-app";
+// export const DB_PORT = process.env.DB_PORT || 3306;
 
-config.js
+
 export const DB_URL = process.env.MYSQL_URL || "";
--- BackEnd/src/db.js
+++ BackEnd/src/db.js
@@ -1,12 +1,26 @@
 import { createPool } from "mysql2/promise";
-import { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD, DB_PORT } from "./config.js";
+import {
+  MYSQLPORT,
+  MYSQLDATABASE,
+  MYSQLPASSWORD,
+  MYSQLUSER,
+  MYSQLHOST,
+} from "./config.js";
 export const pool = createPool({
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_NAME,
-  port: DB_PORT,
+  host: MYSQLHOST,
+  user: MYSQLUSER,
+  password: MYSQLPASSWORD,
+  database: MYSQLDATABASE,
+  port: MYSQLPORT,
 });
+// export const pool = createPool({
+//   host: DB_HOST,
+//   user: DB_USER,
+//   password: DB_PASSWORD,
+//   database: DB_NAME,
+//   port: DB_PORT,
+// });
+
 // import { createPool } from "mysql2/promise";
 // import { DB_URL } from "./config.js";
 
