e31a31c8fe8ba3eb36995b501ec3ae98e452c1a4
FLRSscript <flrsscript@users.noreply.github.com>
2024-08-04 12:50:57 -0600

    responsive iphone

--- FrontEnd/src/components/Header.jsx
+++ FrontEnd/src/components/Header.jsx
@@ -17,7 +17,7 @@ export default function Header() {
       </div>
       <div className="header-image-container">
         <div className="water-bg"></div>
-        <img className="header-image" src="/assets/shampoo2.png" alt="shampoo" />
+        <img className="header-image" src="/assets/shampoo.webp" alt="shampoo" />
       </div>
     </header>
   );
