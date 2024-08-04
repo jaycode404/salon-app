284904b40b33246b3fc4acc913a069e1195691e5
FLRSscript <flrsscript@users.noreply.github.com>
2024-08-04 12:56:26 -0600

    responsive iphone

--- FrontEnd/src/components/Testimoniales.jsx
+++ FrontEnd/src/components/Testimoniales.jsx
@@ -5,10 +5,10 @@ export default function Testimoniales() {
   return (
     <div className="testimoniales-container">
       <h3>Nuestros clientes opinan:</h3>
-      {/* <div
+      <div
         class="elfsight-app-da7a7172-5af1-4d04-afa1-26bf6469256b"
         data-elfsight-app-lazy
-      ></div> */}
+      ></div>
     </div>
   );
 }
